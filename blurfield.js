16a2a3f2937de4954442fff38cd5ab647ad86ea6
ikalangita <ikalangitahaja@gmail.com>
2015-01-06 11:30:54 +0100

    Mise en forme v2
    Mise en forme v2

--- blurfield.js
+++ blurfield.js
@@ -1,17 +1,17 @@
 function blurField( el ){
 	el.each(function () {
-			    // store default value
-			    var v = this.value;
+	    // store default value
+	    var v = this.value;
 
-			    $(this).blur(function () {
-			        // if input is empty, reset value to default 
-			        if (this.value.length == 0) this.value = v;
-			    }).focus(function () {
-			        // when input is focused, clear its contents
-			        this.value = "";
-			    }); 
-			});
+	    $(this).blur(function () {
+	        // if input is empty, reset value to default 
+	        if (this.value.length == 0) this.value = v;
+	    }).focus(function () {
+	        // when input is focused, clear its contents
+	        this.value = "";
+	    }); 
+	});
 }
 
 // === Appel de la fonction 
-blurField( $('input, textarea, select').not(':input[type=button], :input[type=submit], :input[type=reset]') );
+blurField( $('input, textarea').not(':input[type=button], :input[type=submit], :input[type=reset]') );
